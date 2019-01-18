a95008af39b0c9e6b5c2850f9d709feecc564398
pk <472137467@qq.com>
2019-01-18 18:57:58 +0800

    把本地工程添加到git

---
+++ git_test/src/com/itheima/git/Testgit.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * @Description: java类作用描述
+ * @Author: PK
+ * @CreateDate: 2019/1/18 18:51
+ * @Version: 1.0
+ */
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("this is git");
+    }
+}
